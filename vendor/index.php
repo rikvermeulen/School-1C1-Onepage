b94f36d545322ff324acb184a2b2da629585216e
Rik Vermeulen <rik.vermeulen.1997@live.nl>
2019-11-16 20:48:33 +0100

    html-file

---
+++ vendor/index.php
@@ -0,0 +1,183 @@
+
+<!doctype html>
+<html lang="en"><head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+        <meta name="csrf-token" content="zyTR6INZFAJ1jny7nMtJXSxs3rSV4SKl1lrfUHOk">
+
+        <title>Where you at?</title>
+        <meta name="" content="">
+        <meta name="title" content="onepage">
+        <meta name="description" content="onepage voor CLE1-SPRINT3">
+        <meta name="keywords" content="onepage">
+
+        <meta name="author" content="Rik Vermeulen">
+        <link rel="author" href="humans.txt">
+
+        <meta name="google" content="notranslate">
+        <meta name="theme-color" content="#fff">
+        <meta name="generator" content="PHPStorm">
+
+        <link rel="license" href="copyright.html">
+
+        <!--social-->
+        <meta property="og:url" content="">
+        <meta property="og:type" content="">
+        <meta property="og:title" content="">
+        <meta property="og:description" content="">
+        <meta property="og:image" content="">
+
+        <meta name="twitter:card" content="summary/description">
+        <meta name="twitter:url" content="https://website.com">
+        <meta name="twitter:title" content="Title of website">
+        <meta name="twitter:description" content="description">
+
+        <!--google-->
+        <meta name="robots" content="index, follow">
+        <meta name="revisit-after" content="3 month">
+
+        <!--favicon-->
+        <link rel="apple-touch-icon-precomposed" sizes="57x57" href="http://onepages3.test/image/favicon.ico">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="http://onepages3.test/image/favicon.ico">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="http://onepages3.test/image/favicon.ico">
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="http://onepages3.test/image/favicon.ico">
+        <link rel="apple-touch-icon-precomposed" sizes="60x60" href="http://onepages3.test/image/favicon.ico">
+        <link rel="apple-touch-icon-precomposed" sizes="120x120" href="http://onepages3.test/image/favicon.ico">
+        <link rel="apple-touch-icon-precomposed" sizes="76x76" href="http://onepages3.test/image/favicon.ico">
+        <link rel="apple-touch-icon-precomposed" sizes="152x152" href="http://onepages3.test/image/favicon.ico">
+        <link rel="icon" type="image/png" href="http://onepages3.test/images/fav196.png" sizes="196x196">
+        <link rel="icon" type="image/png" href="http://onepages3.test/images/fav96.png" sizes="96x96">
+        <link rel="icon" type="image/png" href="http://onepages3.test/images/fav32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="http://onepages3.test/images/fav16.png" sizes="16x16">
+        <link rel="icon" type="image/png" href="http://onepages3.test/images/fav128.png" sizes="128x128">
+        <meta name="application-name" content="&nbsp;">
+        <meta name="msapplication-TileColor" content="#FFFFFF">
+        <meta name="msapplication-TileImage" content="http://onepages3.test/image/favicon.ico">
+        <meta name="msapplication-square70x70logo" content="http://onepages3.test/image/favicon.ico">
+        <meta name="msapplication-square150x150logo" content="http://onepages3.test/image/favicon.ico">
+        <meta name="msapplication-wide310x150logo" content="http://onepages3.test/image/favicon.ico">
+        <meta name="msapplication-square310x310logo" content="http://onepages3.test/image/favicon.ico">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="css/app.css">
+
+    </head>
+
+    <body>
+
+
+
+    <a name="anker2"></a>
+<header class="nav-down">
+    <div class="nav">
+        <div class="logo">
+            <h1>TEAM 21</h1>
+        </div>
+        <div class="join">
+            <a>JOIN US</a>
+        </div>
+    </div>
+</header>
+
+    <main class="index">
+        <section class="index-hero">
+            <div class="index-hero-content">
+                <div class="index-hero-content-circle">
+
+                </div>
+                <div class="index-hero-content-title">
+                    <div class="index-hero-content-title-single" style="transform: translate3d(0px, 0px, 0px);">
+                        <h4>This</h4>
+                    </div>
+                    <div class="index-hero-content-title-single" style="transform: translate3d(0px, 0px, 0px);">
+                        <h4>is</h4>
+                    </div>
+                    <div class="index-hero-content-title-single" style="transform: translate3d(0px, 0px, 0px);">
+                        <h4>only</h4>
+                    </div>
+                    <div class="index-hero-content-title-single" style="transform: translate3d(0px, 0px, 0px);">
+                        <h4>Temp</h4>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="index-intro">
+            <div class="wrap">
+                <div class="index-intro-content">
+                    <div class="index-intro-content-about">
+                        <h3>INTRO</h3>
+                        <p>The assignment was to create a robot as a team by joining forces. We have bundled our Adafruits as one with which we could develop something what could never have been foreseen. The performance that our creation delivers is above our expectations where he pushes the competition off with his speed, maneuvers around the enemy and detects something with the detection capability’s before you even know it.</p>
+                    </div>
+                    <div class="index-intro-content-update">
+                        <h3>STAY UPDATES</h3>
+                        <ul>
+                            <li><a href="">Facebook</a></li><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><defs><style>.cls-1{fill:#fff;fill-rule:evenodd;}</style></defs><title>arrow-right</title><g><g><polygon class="cls-1" points="0 288 390.4 288 211.2 467.2 256 512 512 256 256 0 211.2 44.8 390.4 224 0 224 0 288"></polygon></g></g></svg>
+                            <li><a href="">Twitter</a></li><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><defs><style>.cls-1{fill:#fff;fill-rule:evenodd;}</style></defs><title>arrow-right</title><g><g><polygon class="cls-1" points="0 288 390.4 288 211.2 467.2 256 512 512 256 256 0 211.2 44.8 390.4 224 0 224 0 288"></polygon></g></g></svg>
+                            <li><a href="">Dribble</a></li><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><defs><style>.cls-1{fill:#fff;fill-rule:evenodd;}</style></defs><title>arrow-right</title><g><g><polygon class="cls-1" points="0 288 390.4 288 211.2 467.2 256 512 512 256 256 0 211.2 44.8 390.4 224 0 224 0 288"></polygon></g></g></svg>
+                        </ul>
+                    </div>
+                </div>
+                <div class="index-intro-selected">
+                    <h4><a href="#anker1">OUR PROJECT</a></h4>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384.67 384.67"><g data-name="Layer 2"><path fill="#fff" fill-rule="evenodd" d="M0 45.26l276.05 276.05H22.63v63.36h362.04V22.63h-63.36v253.42L45.26 0 0 45.26z" data-name="Layer 1"></path></g></svg>
+                </div>
+            </div>
+        </section>
+        <a name="anker1"></a>
+        <section class="index-project">
+            <div class="index-project-content">
+                <h3>Run from it, dread it, destiny still arrives</h3>
+                <div class="index-project-content-info">
+                    <h4>Faster then lightning Mcqueen</h4>
+                    <h4>steers like Max Verstappen</h4>
+                    <h4>Detected enemy's faster then light it self</h4>
+                </div>
+            </div>
+            <div class="index-project-tiles">
+                <div class="index-project-tiles-item">
+                    <img src="http://onepages3.test/images/ironman1.gif" alt="">
+                </div>
+                <div class="index-project-tiles-item">
+                    <img src="http://onepages3.test/images/ironman1.gif" alt="">
+                </div>
+                <div class="index-project-tiles-item">
+                    <img src="http://onepages3.test/images/ironman1.gif" alt="">
+                </div>
+            </div>
+        </section>
+    </main>
+
+
+    <footer>
+    <div class="wrap">
+        <div class="footer">
+            <div class="footer-title">
+                <h3>SAY HELLO</h3>
+            </div>
+            <div class="footer-info">
+                <div class="footer-info-list">
+                    <label for="">Back to the top</label>
+                    <a href="#anker2">Click here</a>
+                </div>
+                <div class="footer-info-list">
+                    <label for="">JOIN US</label>
+                    <a target="_blank" href="https://www.hogeschoolrotterdam.nl/opleidingen/bachelor/creative-media-and-game-technologies/voltijd/">Hogeschool Rotterdam<br> CMGT</a>
+                </div>
+                <div class="footer-info-list">
+                    <label for="">VISIT US</label>
+                    <a href="">
+                        Wijnhaven 99, 3011 WN Rotterdam</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+
+
+        <script src="js/app.js"></script>
+
+
+</body></html>
